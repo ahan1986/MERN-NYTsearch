1199f04da338703a82e0f83c6511806302b36b66
Handrew <andrew.a.han@gmail.com>
2018-10-12 21:21:38 -0400

    add navbar with js and css

--- react-search-nyt/src/App.js
+++ react-search-nyt/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from './component/navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Navbar />
       </div>
     );
   }
--- react-search-nyt/src/component/navbar/navbar.js
+++ react-search-nyt/src/component/navbar/navbar.js
@@ -1,1 +1,13 @@
 import React from 'react';
+import './navbar.css';
+
+export default class Navbar extends React.Component {
+    render () {
+        return (
+            <div className='navbar'>
+                <div className='title'>New York Times Article Scrubber</div>
+                <div className='subtitle'>Search for and annotate articles of interest!</div>
+            </div>
+        )
+    }
+}

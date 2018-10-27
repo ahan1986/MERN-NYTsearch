9fa866f0ee75c779f8228f6fe321605b9c4f391a
Handrew <andrew.a.han@gmail.com>
2018-10-27 14:44:50 -0400

    create card header and body layout for the results that will come after you input topic and years

--- react-search-nyt/src/component/result/Result.js
+++ react-search-nyt/src/component/result/Result.js
@@ -4,30 +4,37 @@ import { connect } from 'react-redux';
 
 
 class Result extends Component {
-    state = {
-        article: [],
-    }
-
-  render() {
-      console.log(this.props.result);
-    return (
-      <div>
-          {/* without anything in the state in redux, this.props.result will be undefined */}
-        {this.props.result.length == undefined ? (
+    render() {
+        console.log(this.props.result);
+        return (
             <div>
-                Hello
-            </div>
+                {/* without anything in the state in redux, this.props.result will be undefined */}
+                {this.props.result.length == undefined ? (
+                    <div>
+                        {/* if there's nothing in the state, this portion of the div will be displayed */}
+                    </div>
 
-        ) : (
-            this.props.result.map((saved, i) => (
-                <div key={i}>
-                    {/* had to specify author because react could not display the whole object onto the screen */}
-                    {saved.author}
-                </div>
-        )))}
-      </div>
-    )
-  }
+                ) : (
+                        <div className="card">
+                            <div className="card-header">
+                                <h3>Results</h3>
+                            </div>
+
+                            {this.props.result.map((saved, i) => (
+
+                                <div className="card-body" key={i}>
+                                    <div className="card-title">
+                                        {/* had to specify author because react could not display the whole object onto the screen */}
+                                        {saved.title}
+                                    </div>
+                                </div>
+
+                            ))}
+                        </div>
+                    )}
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = state => ({
